cb1416df5a7ab27d13a7d4f97039e3f786045c2c
phuccoder <anhdeptraippt@gmail.com>
2024-07-10 23:13:52 +0700

    create update Order

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -8,21 +8,27 @@ import {
   deleteOrder,
   updateOrderStatus,
   getFilteredOrders,
-  getOrdersPaged
+  getOrdersPaged,
+  updateDeliveryDateOrder,
+  updateOrder,
+  updateCircleShipmentOrder
 } from "../controllers/orderController.js";
 
-import { authenticateToken } from "../middleware/authMiddleware.js";
+import { authenticateToken, isUser, isManager, isUserOrManager } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", authenticateToken, createOrder);
-router.get("/getAllOrders", authenticateToken, getAllOrders);
-router.get("/user/:userId", authenticateToken, getOrdersByUser);
+router.post("/", authenticateToken, isUser, createOrder);
+router.get("/getAllOrders", authenticateToken, isManager, getAllOrders);
+router.get("/user/:userId", authenticateToken, isUser, getOrdersByUser);
 router.get("/:id", authenticateToken, getOrderById);
 router.get("/getByDate/:date", getListOrderByDate);
-router.delete("/:id", authenticateToken, deleteOrder);
-router.patch("/:id/status", authenticateToken, updateOrderStatus);
-router.get("/getOrders/filtered", authenticateToken, getFilteredOrders);
+router.delete("/:id", authenticateToken, isUserOrManager, deleteOrder);
+router.patch("/:id/status", authenticateToken, isManager, updateOrderStatus);
+router.get("/getOrders/filtered", authenticateToken, isManager, getFilteredOrders);
 router.get("/getOrders/paged", authenticateToken, getOrdersPaged);
+router.put("/updateDeliveryDate/:id", authenticateToken, isUserOrManager, updateDeliveryDateOrder);
+router.put("/updateOrder/:id", authenticateToken, isUserOrManager, updateOrder);
+router.put("/updateCircleShipment/:id", authenticateToken, isUserOrManager, updateCircleShipmentOrder);
 
 export default router;
